c7c12ca683a2b7a5a5000a003271fddcc0321198
Ibrahim katari <ibrahim.katary@gmail.com>
2020-12-18 00:53:43 +0200

    adding filter function

---
+++ components/AppSelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+        <label :for="select">what {{ select }} do you want? </label>
+
+        <select
+          @change="$emit('change', $event.target.value)"
+          :id="select" :name="select">
+           <option value="">please selcting a {{ select }} </option>
+           <option value="firsthing">firstting</option>
+           <option v-for="option in selectOptions" :key="option" :value="option">
+             {{ option }}
+           </option>
+        </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      select:{
+        type: String,
+        default: "cuisine"
+      },
+      selectOptions: {
+        type: Array,
+        default: () =>['taco', 'pizza', 'dim sum']
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,17 +1,42 @@
 <template>
-  <div>
-
-    <AppRestaurantifo />
+  <div class="container restaurant">
+      <div class="restaurantheading">
+        <h1>Resturants</h1>
+        <Appselect @change="selectedRestaurant = $event" />
+        </div>
+    <AppRestaurantifo :datasource="filteredRestaurants" />
   </div>
 </template>
 
 <script>
 import AppRestaurantifo from '@/components/AppRestaurantifo.vue';
+import Appselect from '@/components/Appselect.vue';
+    import { mapState } from 'vuex';
+
 
 export default {
   components: {
-    AppRestaurantifo
+    AppRestaurantifo,
+    Appselect
+
   },
+  data() {
+    return{
+      selectedRestaurant: ''
+    }
+  },
+  computed: {
+    ...mapState(["fooddata"]),
+    filteredRestaurants(){
+      if (this.selectedRestaurant) {
+          return this.fooddata.filter(el =>{
+            let name = el.name.toLowerCase();
+            return name.includes(this.selectedRestaurant);
+          })
+      }
+      return this.fooddata;
+    }
+  }
 };
 
 </script>
